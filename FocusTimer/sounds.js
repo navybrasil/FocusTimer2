be4a1949fe199593dd1ce95dce5422e9ac54eb4e
navybrasil <navybrasil@gmail.com>
2024-08-03 21:39:50 -0300

    refactor code

--- FocusTimer/sounds.js
+++ FocusTimer/sounds.js
@@ -1,9 +1,5 @@
 export const buttonPressAudio = new Audio("./assets/button-press.wav");
-
 export const kitchenTimer = new Audio("./assets/kichen-timer.mp3");
-
-// export const bgAudio = new Audio("./assets/bg-audio.mp3");
-
 export const musicForest = new Audio("/assets/Floresta.wav");
 export const musicRain = new Audio("/assets/Chuva.wav");
 export const musicCafe = new Audio("/assets/Cafeteria.wav");
